5f33dd4d33b252b55c4dc0ad48f357f88aec600e
arnieafk <159774676+arnieafk@users.noreply.github.com>
2026-05-09 20:14:18 +0800

    Initial commit

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   timezone: '+08:00',
-  dateStrings: false,
+  dateStrings: true,
   ssl: {
     rejectUnauthorized: false,
   },
